e7997122c2391696cbef791988113d16b65ca8ec
Lukasz Jurczynski <l.jurczynski@gmail.com>
2022-01-05 15:31:16 +0100

    Add empty header component

--- pegaworld22/src/app/app.module.ts
+++ pegaworld22/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
